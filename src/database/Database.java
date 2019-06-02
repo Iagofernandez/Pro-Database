6399e6dfd98e1222869b6bdf25ff51fcbd54348c
X541 <X541@LAPTOP-QJCFQ4FC>
2019-06-02 11:51:10 +0200

    Clase main sencillita

---
+++ src/database/Database.java
@@ -0,0 +1,36 @@
+
+package database;
+
+import Metodos.Metodos;
+import static Metodos.Metodos.createNewDatabase;
+import static Metodos.Metodos.createNewTable;
+import Paleta.ConsolaDB;
+
+/**
+ *
+ * @author X541
+ */
+public class Database {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         createNewDatabase("testeo.db");
+
+        ConsolaDB consola = new ConsolaDB();
+
+        consola.setVisible(true);
+
+        
+
+
+
+        
+
+        createNewTable();
+
+        Metodos bd = new Metodos();
+    }
+    
+}
